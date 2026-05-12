f0e8724db2b21b3259d86435ff957ead13145a85
Thiago H O S <thiago2006_sales@hotmail.com>
2026-05-12 11:29:18 -0300

    Configurei a classe Usuario

--- sistemabiblioteca/src/model/Usuario.java
+++ sistemabiblioteca/src/model/Usuario.java
@@ -1,5 +1,40 @@
 package model;
 
-public class Usuario {
+import exceptions.UsuarioInvalidoException;
 
+public class Usuario {
+	//Atributos
+	private String nome;
+	private String email;
+	private String telefone;
+
+	// CONSTRUTORES
+	public Usuario(String nome, String email, String telefone) throws UsuarioInvalidoException {
+		if (nome == null || nome.trim().isEmpty()) {
+			throw new UsuarioInvalidoException("\t O nome do usuário não ser vázio.");
+		}
+		if (email == null || email.trim().isEmpty()) {
+			throw new UsuarioInvalidoException("\t O email não pode ser vázio");
+		}
+		if (telefone == null || telefone.trim().isEmpty()) {
+			throw new UsuarioInvalidoException("\t O telefone não pode ser vázio");
+		}
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	// Getters
+	public String nome() {
+		return nome;
+	}
+
+	public String email() {
+		return email;
+
+	}
+
+	public String telefone() {
+		return telefone;
+	}
 }
